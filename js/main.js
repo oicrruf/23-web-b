b166ee8332df11f86f7e6d40cb496b3bd02bb7a7
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-02-13 21:26:18 -0600

    chore: change message on screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Welcome ATM")
+const screen = document.querySelector(".atm-system__screen");
+const inputCard = document.querySelector(".atm-system__card");
+
+screen.innerText = "Bienvenido al ATM";
+
+inputCard.addEventListener("click", function () {
+  alert("Insertó tarjeta");
+});
